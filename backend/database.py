b7d03fdf61d4ae5da5981b09d54231bd07014b44
EmmaRichardson6148 <remma9443@gmail.com>
2025-03-19 19:06:37 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Ecr05080%21@localhost:3306/product_reviews" Connect to MySQL database
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./test.db")
 
 # Connect to MySQL database
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
